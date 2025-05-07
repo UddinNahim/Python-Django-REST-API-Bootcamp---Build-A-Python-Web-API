52c89c8b30ecb656b98cc472cfdbffed6544e5eb
UddinNahim <nahim.211902019@gmail.com>
2025-05-08 05:27:52 +0600

    session based authentication

--- src/taskful_api/settings.py
+++ src/taskful_api/settings.py
@@ -105,6 +105,11 @@ AUTH_PASSWORD_VALIDATORS = [
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES':[
         'rest_framework.permissions.IsAuthenticatedOrReadOnly'
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES':[
+        #rest Framework authentication classes
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
     ]
 }
 
