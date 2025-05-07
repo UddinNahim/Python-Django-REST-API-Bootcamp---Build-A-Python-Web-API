4cb842d333929908126581a6fedffae2b520fc72
UddinNahim <nahim.211902019@gmail.com>
2025-05-08 04:50:17 +0600

    permission develop and learn

---
+++ src/users/permissions.py
@@ -0,0 +1,21 @@
+from rest_framework import permissions
+
+class IsUserOwnerOrGetAndPostOnly(permissions.BasePermission):
+    """
+    custom permissions for UserViewset to only allow user to edit their own profile .otherwise, get and post only.
+    """
+
+    def has_permission(self,request,view):
+        return True
+
+    def has_object_permission(self,request,view,obj):
+
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        
+        if not request.user.is_anonymous:
+            return request.user == obj
+        
+        return False
+        
+
--- src/users/viewsets.py
+++ src/users/viewsets.py
@@ -1,10 +1,16 @@
+# A ViewSet is a class that defines the logic for a group of related view.
+
 from django.contrib.auth.models import User
 
 from rest_framework import viewsets
 
 from .serializers import UserSerializer
 
+from .permissions import IsUserOwnerOrGetAndPostOnly
+
+
 class UserViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsUserOwnerOrGetAndPostOnly,]
     queryset = User.objects.all() 
     serializer_class = UserSerializer
 
