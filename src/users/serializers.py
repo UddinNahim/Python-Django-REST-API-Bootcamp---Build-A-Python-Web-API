554099219d602ec5add8e0549b82db6d2f915929
UddinNahim <nahim.211902019@gmail.com>
2025-05-11 03:38:00 +0600

    password changes system learning

--- src/users/serializers.py
+++ src/users/serializers.py
@@ -5,6 +5,8 @@ from rest_framework import serializers
 class UserSerializer(serializers.ModelSerializer):
 
     password = serializers.CharField(write_only=True, required=False)
+    old_password = serializers.CharField(write_only=True, required=False)
+
     username = serializers.CharField(read_only=True)
 
     def create(self, validated_data):
@@ -28,7 +30,7 @@ class UserSerializer(serializers.ModelSerializer):
             user.save()
         except Exception as err:
             raise serializers.ValidationError
-
+        return super(UserSerializer,self).update(instance,validated_data)
 
         
     class Meta:
