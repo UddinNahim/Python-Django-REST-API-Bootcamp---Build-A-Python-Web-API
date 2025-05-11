c653a1f59ddf1e49899f5aeac183a34ae2918b4c
UddinNahim <nahim.211902019@gmail.com>
2025-05-11 06:05:20 +0600

    serilizer customization

--- src/users/serializers.py
+++ src/users/serializers.py
@@ -6,9 +6,22 @@ class UserSerializer(serializers.ModelSerializer):
 
     password = serializers.CharField(write_only=True, required=False)
     old_password = serializers.CharField(write_only=True, required=False)
-
     username = serializers.CharField(read_only=True)
 
+
+    def validate(self,data):
+        request_method = self.context['request'].method
+        password = data.get('password',None)
+        if request_method == 'POST':
+            if password == None:
+                raise serializers.ValidationError("Please provide a password")
+
+        elif request_method == 'PATCH' or request_method == 'PUT':
+            old_password = data.get('old_password', None) 
+            if password != None and old_password == None:
+                raise serializers.ValidationError({"info": "Please provide the old password"})
+        return data
+            
     def create(self, validated_data):
         password = validated_data.pop('password')
         user = User.objects.create(**validated_data)
@@ -20,14 +33,15 @@ class UserSerializer(serializers.ModelSerializer):
     def update(self,instance,validated_data):
         try:
             user = instance
-            password = validated_data.pop('password')
-            old_password = validated_data.pop('old_password')
+            if 'password' in validated_data:
+                password = validated_data.pop('password')
+                old_password = validated_data.pop('old_password')
 
-            if user.check_password(old_password):
-                user.set_password(password)
-            else:
-                raise Exception("old password is incorrect")
-            user.save()
+                if user.check_password(old_password):
+                    user.set_password(password)
+                else:
+                    raise Exception("old password is incorrect")
+                user.save()
         except Exception as err:
             raise serializers.ValidationError
         return super(UserSerializer,self).update(instance,validated_data)
