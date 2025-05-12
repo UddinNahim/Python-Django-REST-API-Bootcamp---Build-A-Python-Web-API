071030abeb3b0aca588b0bf43a2a76b45f2c9f7e
UddinNahim <nahim.211902019@gmail.com>
2025-05-12 06:12:07 +0600

    customizing profileviewsets

--- src/users/permissions.py
+++ src/users/permissions.py
@@ -2,7 +2,7 @@ from rest_framework import permissions
 
 class IsUserOwnerOrGetAndPostOnly(permissions.BasePermission):
     """
-    custom permissions for UserViewset to only allow user to edit their own profile .otherwise, get and post only.
+    custom permissions for UserViewset to only allow user to edit their own User .otherwise, get and post only.
     """
 
     def has_permission(self,request,view):
@@ -17,5 +17,23 @@ class IsUserOwnerOrGetAndPostOnly(permissions.BasePermission):
             return request.user == obj
         
         return False
+    
+class IsProfileOwnerOrReadOnly(permissions.BasePermission):
+    """
+    custom permissions for Profile Viewset to only allow user to edit their own profile .otherwise, get and post only.
+    """
+        
+    def has_permission(self, request, view):
+        return True
+    
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        
+        if not request.user.is_anonymous:
+            return request.user.profile == obj
+        
+        return False
+
         
 
--- src/users/viewsets.py
+++ src/users/viewsets.py
@@ -2,11 +2,11 @@
 
 from django.contrib.auth.models import User
 
-from rest_framework import viewsets
+from rest_framework import viewsets , mixins
 
 from .serializers import UserSerializer , ProfileSerializer
 
-from .permissions import IsUserOwnerOrGetAndPostOnly
+from .permissions import IsUserOwnerOrGetAndPostOnly ,IsProfileOwnerOrReadOnly
 from .models import Profile
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -16,7 +16,8 @@ class UserViewSet(viewsets.ModelViewSet):
 
 
 
-class ProfileViewSet(viewsets.ModelViewSet):
+class ProfileViewSet(viewsets.GenericViewSet,mixins.RetrieveModelMixin,mixins.UpdateModelMixin):
+    permission_classes = [IsProfileOwnerOrReadOnly]
     queryset = Profile.objects.all()
     serializer_class = ProfileSerializer
 
